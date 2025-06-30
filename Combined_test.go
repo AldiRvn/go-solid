cfab4e45629726c04b9d5ea94dcccaa14028b491
Aldi.Rismawan <aldirvn4@gmail.com>
2025-06-30 15:57:13 +0700

    ✨ feat(Liskov Substication Principle): LSP

--- Combined_test.go
+++ Combined_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"testing"
@@ -34,13 +33,23 @@ type AuthenticationService struct {
 }
 
 type Authenticator interface {
-	Authenticate(user User, password string) bool
+	Authenticate(user User, credentials any) bool
 }
 
 type PasswordAuthenticator struct{}
 
-func (pa *PasswordAuthenticator) Authenticate(user User, password string) bool {
+func (pa *PasswordAuthenticator) Authenticate(user User, credentials any) bool {
+	password, ok := credentials.(string)
+	if !ok {
+		return false
+	}
 	return user.Password == password
+}
+
+type FingerprintAuthenticator struct{}
+
+func (fa *FingerprintAuthenticator) Authenticate(user User, credentials any) (res bool) {
+	return true
 }
 
 func NewAuthenticationService(userRepository *UserRepository) *AuthenticationService {
@@ -54,22 +63,12 @@ func (as *AuthenticationService) AddAuthenticator(authenticator Authenticator) {
 	as.authenticator = append(as.authenticator, authenticator)
 }
 
-func (as *AuthenticationService) AuthenticationUser(user User, password string) (ok bool) {
+func (as *AuthenticationService) AuthenticationUser(user User, credentials any) (ok bool) {
 	for _, authenticator := range as.authenticator {
-		if authenticator.Authenticate(user, password) {
+		if authenticator.Authenticate(user, credentials) {
 			return true
 		}
 	}
-	return
-}
-
-func (as *AuthenticationService) getUserByUsername(username string) (res User, err error) {
-	for _, user := range as.userRepository.Users {
-		if user.Username == username {
-			return user, nil
-		}
-	}
-	err = errors.New("data not found.")
 	return
 }
 
@@ -85,13 +84,21 @@ func Test_Combined(t *testing.T) {
 
 	authService := NewAuthenticationService(userRepository)
 	authService.AddAuthenticator(&PasswordAuthenticator{})
+	authService.AddAuthenticator(&FingerprintAuthenticator{})
 
 	password := "Doe"
-	isAuthentication := authService.AuthenticationUser(user, password)
+	isAuthenticatedPassword := authService.AuthenticationUser(user, password)
 
-	if isAuthentication {
-		log.Println("Welcome", user.Username)
+	if isAuthenticatedPassword {
+		log.Println("Password authentication success, welcome", user.Username)
 	} else {
-		log.Println("Login failed")
+		log.Println("Password authentication failed, invalid credentials")
+	}
+
+	isAuthenticatedFingerprint := authService.AuthenticationUser(user, "fingerprint_data")
+	if isAuthenticatedFingerprint {
+		log.Println("Fingerprint authentication success, welcome", user.Username)
+	} else {
+		log.Println("Fingerprint authentication failed, invalid credentials")
 	}
 }
